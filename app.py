a58274814c737624199452308f68a43d359cbe3e
PedroMorales2 <72403333@usat.pe>
2024-09-23 00:11:24 -0500

    commit

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ def hello_world():
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/services')
+def services():
+    return render_template('services.html')
